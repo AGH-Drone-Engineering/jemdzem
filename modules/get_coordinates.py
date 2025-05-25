1a29212a188631ecc9651ace9cf7b2111401dd74
theSPGN <spgn1226@gmail.com>
2025-05-25 14:36:53 +0200

    cam_mat configuration added

--- modules/get_coordinates.py
+++ modules/get_coordinates.py
@@ -1,9 +1,34 @@
 import math
 from geographiclib.geodesic import Geodesic
 from typing import Tuple
+import numpy as np
 
-def degrees_to_d_m_s(degrees:float) -> Tuple[int, float]:
-    """Converts decimal degrees to degrees, minutes, and seconds.
+
+def calibration(calib_data_path: str) -> Tuple[np.ndarray]:
+    """
+    Function with give path to camera calibration returns tuple
+    of calibration data
+
+    Args:
+        calib_data_path - path to calibration file .npz
+
+    Returns:
+        cam_mat, dist_coef, r_vectors, t_vectors - tuple of
+        numpy.ndarrays for further calibration and length
+        estimation
+    """
+    calib_data = np.load(calib_data_path)
+    cam_mat = calib_data["camMatrix"]
+    dist_coef = calib_data["distCoef"]
+    r_vectors = calib_data["rVector"]
+    t_vectors = calib_data["tVector"]
+
+    return cam_mat, dist_coef, r_vectors, t_vectors
+
+
+def degrees_to_d_m_s(degrees: float) -> Tuple[int, float]:
+    """
+    Function converts decimal degrees to degrees, minutes, and seconds.
 
     Args:
         degrees - floating value of geo postion in one var
@@ -17,8 +42,10 @@ def degrees_to_d_m_s(degrees:float) -> Tuple[int, float]:
     s = (minutes_decimal - m) * 60
     return d, m, s
 
-def d_m_s_to_degrees(d:int, m:int, s:float) -> float:
-    """Converts degrees, minutes, and seconds to decimal degrees.
+
+def d_m_s_to_degrees(d: int, m: int, s: float) -> float:
+    """
+    Function converts degrees, minutes, and seconds to decimal degrees.
 
     Args:
         d - degrees
@@ -31,12 +58,14 @@ def d_m_s_to_degrees(d:int, m:int, s:float) -> float:
     degrees = d + (m / 60) + (s / 3600)
     return degrees
 
+
 def pixels_to_meters(
     pixels: int,  # distance in pixels that will be converted to meters
     altitude: float,  # drone altitude
     img_width: int,  # image width in pixels
-    focal_length: float,  # focal length (camera dependency)
-    sensor_width: float,  # sensor width (camera dependency)
+    cam_mat: np.ndarray = None,  # calibration camera matrix
+    focal_length: float = None,  # focal length (camera dependency)
+    sensor_width: float = None,  # sensor width (camera dependency)
 ) -> float:  # meters received from pixels
     """
     Function estimates the meter distance based on pixel distance
@@ -46,13 +75,18 @@ def pixels_to_meters(
         pixels - distance in pixels that needs to be converted to meters
         altitude - altitude of drone in meters
         img_width - image width of a frame in pixels
-        focal_length - camera parameter provided by manufacturer
-        sensor_width - camera parameter provided by manufacturer
+        cam_mat - calibration camera matrix created by testing camera with
+        chessboard squares (https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html)
+        focal_length - camera parameter provided by manufacturer default None because cam_mat is used
+        sensor_width - camera parameter provided by manufacturer default None because cam_mat is used
     Returns:
         meters - converted pixels to the meters unit
     """
-    meters_per_pixel = (altitude * sensor_width) / (focal_length * img_width)
-
+    if cam_mat is None:
+        meters_per_pixel = (altitude * sensor_width) / (focal_length * img_width)
+    else:
+        fx = cam_mat[0][0]
+        meters_per_pixel = altitude / fx
     meters = pixels * meters_per_pixel
     return meters
 
@@ -64,8 +98,9 @@ def translate(
     angle=0.0,
     img_height=3648.0,
     img_width=5472.0,
-    focal_length=23.0,
-    sensor_width=25.4,
+    cam_mat=None,
+    focal_length=None,
+    sensor_width=None,
 ):
     """
     Function calculates the offset in meters from the image center for a detected object,
@@ -79,8 +114,10 @@ def translate(
         angle - rotation angle of the image (radians), default 0.0
         img_height - image height in pixels, default 3648.0
         img_width - image width in pixels, default 5472.0
-        focal_length - camera focal length, default 23.0
-        sensor_width - camera sensor width, default 25.4
+        cam_mat - calibration camera matrix created by testing camera with
+        chessboard squares (https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html)
+        focal_length - camera focal length, default None because cam_mat is used
+        sensor_width - camera sensor width, default None because cam_mat is used
 
     Returns:
         delta_x_meters: Offset in meters from the image center along the X axis.
@@ -89,10 +126,20 @@ def translate(
     x_n = x * math.cos(angle) - y * math.sin(angle)
     y_n = x * math.sin(angle) + y * math.cos(angle)
     x_center = pixels_to_meters(
-        img_width / 2.0, altitude, img_width, focal_length, sensor_width
+        img_width / 2.0,
+        altitude,
+        img_width,
+        cam_mat=cam_mat,
+        focal_length=focal_length,
+        sensor_width=sensor_width,
     )
     y_center = pixels_to_meters(
-        img_height / 2.0, altitude, img_width, focal_length, sensor_width
+        img_height / 2.0,
+        altitude,
+        img_width,
+        cam_mat=cam_mat,
+        focal_length=focal_length,
+        sensor_width=sensor_width,
     )
     delta_x_meters = x_n - x_center
     delta_y_meters = -(y_n - y_center)
@@ -146,12 +193,26 @@ def unit_test_get_coordinates():
     focal_length = 23.0
     sensor_width = 25.4
 
+    cam_mat, dist_coef, r_vectors, t_vectors = calibration(
+        "../camera_dependencies/Yuneec.npz"
+    )
+
     # Test pixels_to_meters for center (should be 0 offset)
     meters_x = pixels_to_meters(
-        x_pixel, altitude, img_width, focal_length, sensor_width
+        x_pixel,
+        altitude,
+        img_width,
+        cam_mat=cam_mat,
+        focal_length=focal_length,
+        sensor_width=sensor_width,
     )
     meters_y = pixels_to_meters(
-        y_pixel, altitude, img_width, focal_length, sensor_width
+        y_pixel,
+        altitude,
+        img_width,
+        cam_mat = cam_mat,
+        focal_length=focal_length,
+        sensor_width=sensor_width,
     )
     print(f"meters_x: {meters_x}, meters_y: {meters_y}")
 
@@ -163,8 +224,9 @@ def unit_test_get_coordinates():
         angle,
         img_height,
         img_width,
-        focal_length,
-        sensor_width,
+        cam_mat=cam_mat,
+        focal_length=focal_length,
+        sensor_width=sensor_width,
     )
     print(f"delta_x: {delta_x}, delta_y: {delta_y}")
     assert abs(delta_x) < 1e-6, "delta_x should be near zero for image center"
@@ -180,10 +242,20 @@ def unit_test_get_coordinates():
     x_pixel2 = 3000
     y_pixel2 = 2000
     meters_x2 = pixels_to_meters(
-        x_pixel2, altitude, img_width, focal_length, sensor_width
+        x_pixel2,
+        altitude,
+        img_width,
+        cam_mat=cam_mat,
+        focal_length=focal_length,
+        sensor_width=sensor_width,
     )
     meters_y2 = pixels_to_meters(
-        y_pixel2, altitude, img_width, focal_length, sensor_width
+        y_pixel2,
+        altitude,
+        img_width,
+        cam_mat=cam_mat,
+        focal_length=focal_length,
+        sensor_width=sensor_width,
     )
 
     delta_x2, delta_y2 = translate(
@@ -193,8 +265,9 @@ def unit_test_get_coordinates():
         angle,
         img_height,
         img_width,
-        focal_length,
-        sensor_width,
+        cam_mat=cam_mat,
+        focal_length=focal_length,
+        sensor_width=sensor_width,
     )
     new_lat2, new_lng2 = calculate_new_coordinates(lat, lng, delta_x2, delta_y2)
     print(f"Offset lat/lng: {new_lat2}, {new_lng2}")
